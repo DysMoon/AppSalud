10753834b2b9bd64e1d565c3e283905675245b3a
Diana Huaman <120173943+DysMoon@users.noreply.github.com>
2025-04-09 06:37:42 -0500

    detalles api

--- app/src/main/java/Network/RetrofitClient.java
+++ app/src/main/java/Network/RetrofitClient.java
@@ -6,8 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static Retrofit retrofit = null;
 
-    // URL base de tu API (asegúrate de que termine con '/')
-    private static final String BASE_URL = "http://dys.somee.com/api/";
+    private static final String BASE_URL = "http://bazurha.somee.com/api/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
